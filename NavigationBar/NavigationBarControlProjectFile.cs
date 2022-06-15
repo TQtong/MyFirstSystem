70508aca5abbf9451d26557a7dde8210d28ac1c2
TQ <tangqi@southzn.com>
2022-06-15 16:21:15 +0800

    book添加

---
+++ NavigationBar/NavigationBarControlProjectFile.cs
@@ -0,0 +1,24 @@
+﻿using NavigationBar.ViewModels;
+using NavigationBar.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationBar
+{
+    public class NavigationBarControlProjectFile : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<BookView, BookViewModel>();
+        }
+    }
+}
